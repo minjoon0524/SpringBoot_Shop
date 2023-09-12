a4a78ff584c34b57dfc17fa206fce2c24bbd5eae
MINJUN YU <minjoon0524@gmail.com>
2023-09-12 16:14:18 +0900

    ItemRepository Test

---
+++ src/test/java/inhatc/spring/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,38 @@
+package inhatc.spring.shop.repository;
+
+import inhatc.spring.shop.constant.ItemSellStatus;
+import inhatc.spring.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+class ItemRepositoryTest {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest(){
+        Item item = Item.builder()
+                .itemNm("테스트 상품")
+                .price(10000)
+                .itemDetail("테스트 상품 상세 설명")
+                .itemSellStatus(ItemSellStatus.SELL)
+                .stockNumber(100)
+                .regTime(LocalDateTime.now())
+                .updateTime(LocalDateTime.now())
+                .build();
+
+        System.out.println("==============================================Item : "+item);
+        Item savedItem = itemRepository.save(item);
+        System.out.println("==============================================Item : "+savedItem);
+
+
+    }
+
+}
