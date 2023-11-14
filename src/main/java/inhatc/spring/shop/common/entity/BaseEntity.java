09cfa7965c1a2c9d9dbe3667a6de02d4acd08886
MINJUN YU <minjoon0524@gmail.com>
2023-11-14 14:40:16 +0900

    Common Class

--- src/main/java/inhatc/spring/shop/common/entity/BaseEntity.java
+++ src/main/java/inhatc/spring/shop/common/entity/BaseEntity.java
@@ -1,6 +1,5 @@
 package inhatc.spring.shop.common.entity;
 
-import inhatc.spring.shop.config.AuditorAwareImpl;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -8,8 +7,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@EntityListeners(value = {AuditorAwareImpl.class}) // AuditorAwareImpl 사용하여 등록이나 수정 시간 자동으로 처리
+@EntityListeners(value = {AuditingEntityListener.class}) // AuditingEntityListener 사용하여 등록이나 수정 시간 자동으로 처리
 @MappedSuperclass // 해당클래스를 상속받는 클래스에게 속성만 제공
 @Getter
 @Setter
--- src/main/java/inhatc/spring/shop/common/entity/BaseTimeEntity.java
+++ src/main/java/inhatc/spring/shop/common/entity/BaseTimeEntity.java
@@ -8,12 +8,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 //공통으로 사용하는 클래스
 //추상클래스(단독으로 객체를 만들지 못 하게 한다.)
 //감시를 할 수 있는 용도
-@EntityListeners(value = {AuditorAwareImpl.class}) // AuditorAwareImpl 사용하여 등록이나 수정 시간 자동으로 처리
+@EntityListeners(value = {AuditingEntityListener.class}) // AuditingEntityListener 사용하여 등록이나 수정 시간 자동으로 처리
 @MappedSuperclass // 해당클래스를 상속받는 클래스에게 속성만 제공
 @Getter
 @Setter
